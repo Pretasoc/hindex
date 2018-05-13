3abceb56e33b88643e30973cec163448d7cdff8c
Jonas Brune <janjonas.brune@gmail.com>
2018-05-13 22:21:50 +0200

    add JUNIT Test

--- src/test/HIndexCalculation.java
+++ src/test/HIndexCalculation.java
@@ -1,21 +1,21 @@
 package test;
 
-
 import java.io.FileNotFoundException;
 import java.util.LinkedList;
 import java.util.List;
 
 import org.junit.Assert;
-import org.junit.jupiter.api.Test;
 
 import realAufgabe1.Controller;
 import realAufgabe1.PaperCitation;
 
-class HIndexCalculation {
+import org.junit.Test;
+
+public class HIndexCalculation {
 
 	@Test
 	//Returns list of all matched Persons
-	void testInputHit() throws FileNotFoundException {
+	public void testInputHit() throws FileNotFoundException {
 		List<PaperCitation> goal = new LinkedList<>();
 		goal.add(new PaperCitation("F. Kortum", "The SE-Supreme - Unpublished Studies Part 1", 3));
 		goal.add(new PaperCitation("F. Kortum", "The SE-Supreme - Unpublished Studies Part 2", 4));
@@ -37,7 +37,7 @@ class HIndexCalculation {
 	
 	@Test
 	//Wrong input returns empty list
-	void testInputMiss() throws FileNotFoundException {
+	public void testInputMiss() throws FileNotFoundException {
 		Assert.assertEquals(new LinkedList(), (new Controller()).getListPaperCitationFromName("Falsche Eingabe"));
 	}
 
